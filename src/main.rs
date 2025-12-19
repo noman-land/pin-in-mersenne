79a8b402bddbf9f994ae7297a2c66ee6107e77b8
Noman <noman@noman.land>
2025-12-19 18:08:53 -0500

    Nice

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::str::from_utf8;
+
+fn main() {
+    const PIN_LENGTH: usize = 6;
+    let mut pins = vec![0u8; 1_000_000];
+
+    fs::read_to_string("data/prime.txt")
+        .expect("Can't read file")
+        .bytes()
+        .filter(|b| b.is_ascii_digit())
+        .collect::<Vec<u8>>()
+        .windows(PIN_LENGTH)
+        .enumerate()
+        .for_each(|(_, window)| {
+            let pin = from_utf8(window)
+                .expect("Couldn't convert to string")
+                .parse::<usize>()
+                .expect("Couldn't convert to usize");
+            pins[pin] += 1;
+        });
+
+    println!(
+        "Count: {}",
+        pins.iter().filter(|count| **count == 21).count()
+    )
+}
